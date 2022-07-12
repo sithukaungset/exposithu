912a62b80d6b7c6cb71ba7fcd331b0ab7f3472b8
Sithu Kaungset <sithukaungset@Sithus-MacBook-Air.local>
2022-07-12 21:36:40 +0900

    expo commit

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+// View => UIView (in IOS)
+// View => Android view 
+// We have to change the view and other similar functions in native language alike
+
+
 export default function App() {
+  console.log("App executed")
+  let x = 1;
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello React native I am sithu!</Text>
       <StatusBar style="auto" />
     </View>
   );
