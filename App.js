de6291780235ee058f7568a71fbb5478d783ce91
Sithu Kaungset <sithukaungset@Sithus-MacBook-Air.local>
2022-07-13 23:34:59 +0900

    core component

--- App.js
+++ App.js
@@ -23,7 +23,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
     justifyContent: "center",
     alignItems:"center"
    
